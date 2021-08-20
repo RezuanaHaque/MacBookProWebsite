22381016f3b726d3747fb52880ef79f89e66b574
megha-r-prog <rezuanahaque0@gmail.com>
2021-08-21 02:37:21 +0600

    total calculation

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // memory buttons
-// 8gb
 document.getElementById('mem8gb').addEventListener('click', function () {
     memoryextra(false);
 });
@@ -14,5 +13,58 @@ function memoryextra(isextramemory) {
     else {
         extra_mem.innerText = 180;
     }
+    calculatetotal();
+}
+// storage buttons
+document.getElementById('storage256gb').addEventListener('click', function () {
+    storageextra('256GB');
 
+});
+document.getElementById('storage512gb').addEventListener('click', function () {
+    storageextra('512GB');
+
+});
+document.getElementById('storage1tb').addEventListener('click', function () {
+    storageextra('1TB');
+
+});
+
+function storageextra(isextrastorage) {
+    const extra_storage = document.getElementById('extrastorage');
+    if (isextrastorage == '256GB') {
+        extra_storage.innerText = 0;
+    }
+    if (isextrastorage == '512GB') {
+        extra_storage.innerText = 100;
+    }
+    if (isextrastorage == '1TB') {
+        extra_storage.innerText = 180;
+    }
+    calculatetotal();
+}
+// delivery charges
+document.getElementById('freedelivery').addEventListener('click', function () {
+    deliveryextra(false, 'extradelivery');
+});
+document.getElementById('deliverycharge').addEventListener('click', function () {
+    deliveryextra(true, 'extradelivery');
+});
+function deliveryextra(isdeliverycharge, field) {
+    const delivery_charge = document.getElementById(field);
+    if (isdeliverycharge == false) {
+        delivery_charge.innerText = 0;
+    }
+    else {
+        delivery_charge.innerText = 20;
+    }
+    calculatetotal();
+}
+// total payment
+function calculatetotal() {
+    const basic_price = document.getElementById('basicprice').innerText;
+    const extra_mem = document.getElementById('extramem').innerText;
+    const extra_storage = document.getElementById('extrastorage').innerText;
+    const delivery_charge = document.getElementById('extradelivery').innerText;
+    const total_price = document.getElementById('totalprice');
+    total_price.innerText = parseFloat(basic_price) + parseFloat(extra_mem) + parseFloat(extra_storage) + parseFloat(delivery_charge);
 }
